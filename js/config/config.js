aa9fdf4c908ebaca819d6c0bb4e611ffdf5669bf
juanmackie <juan.mackie@gmail.com>
2025-06-08 13:50:32 +1000

    Update config.js
    Updated line 21 
    
    FROM: models/gemini-2.0-flash-exp
    
    TO: models/gemini-2.5-flash-preview-native-audio-dialog

--- js/config/config.js
+++ js/config/config.js
@@ -18,7 +18,7 @@ const thresholds = {
 }
 
 export const getConfig = () => ({
-    model: 'models/gemini-2.0-flash-exp',
+    model: 'models/gemini-2.5-flash-preview-native-audio-dialog',
     generationConfig: {
         temperature: parseFloat(localStorage.getItem('temperature')) || 1.8,
         top_p: parseFloat(localStorage.getItem('top_p')) || 0.95,
